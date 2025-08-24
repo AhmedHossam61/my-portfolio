a44b75312efc24bfb231e149ea71bae3a6506929
AhmedHossam61 <ahmedhoss.2023@gmail.com>
2025-08-25 01:55:28 +0300

    Update Hero component with full name and correct image source

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -31,7 +31,7 @@ const Hero = () => {
             </div>
           </div>
           <div className="hero-image">
-            <img src="/Personal-photos/graduation.jpg" alt="Ahmed Hossam Abdelrahman" />
+            <img src="/Personal-photos/graduation.jpg" alt="Ahmed Hossam " />
           </div>
         </div>
       </div>
